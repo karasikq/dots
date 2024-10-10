04034dd5644dd34ec551b749bd58bcc9963748c2
karasikq <romastepanuik@gmail.com>
2024-10-10 12:54:15 +0300

    new install scripts

--- create-simlink.sh
+++ create-simlink.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-ln -s -T $HOME/.config/zsh/.zshrc $HOME/.zshrc
+ZSHRC="$HOME/.zshrc"
+NEW_ZSHRC="$HOME/.config/zsh/.zshrc"
+
+if [ -f "$ZSHRC" ]; then
+    mv "$ZSHRC" "${ZSHRC}.backup"
+    echo "Backup of .zshrc created at ${ZSHRC}.backup."
+fi
+
+ln -s -T "$NEW_ZSHRC" "$ZSHRC"
+echo "Symbolic link created: $NEW_ZSHRC -> $ZSHRC"
--- install.sh
+++ install.sh
@@ -1,4 +1,61 @@
-#!/bin/sh
-cp -r .config/zsh $HOME/.config
-cp -r .config/nvim $HOME/.config
+#!/bin/bash
+
+echo "Updating submodules..."
+git submodule update --init --recursive
+
+ZSH_DIR="$HOME/.config/zsh"
+NVIM_DIR="$HOME/.config/nvim"
+
+show_menu() {
+    echo "Select an option:"
+    echo "1. Override directory"
+    echo "2. Backup old directory with .backup extension"
+    echo "3. Abort"
+    read -p "Enter your choice [1-3]: " choice
+    return $choice
+}
+
+install() {
+    cp -r .config/zsh "$HOME/.config"
+    cp -r .config/nvim "$HOME/.config"
+    echo "Installed."
+}
+
+if [ -d "$ZSH_DIR" ] || [ -d "$NVIM_DIR" ]; then
+    if [ -d "$ZSH_DIR" ]; then
+        echo "zsh config already exists"
+    fi
+    if [ -d "$NVIM_DIR" ]; then
+        echo "nvim config already exists"
+    fi
+    show_menu
+    choice=$?
+
+    case $choice in
+        1)
+            install
+            ;;
+        2)
+            if [ -d "$ZSH_DIR" ]; then
+                mv "$ZSH_DIR" "${ZSH_DIR}.backup"
+                echo "Backup of zsh directory created at $ZSH_DIR.backup."
+            fi
+            if [ -d "$NVIM_DIR" ]; then
+                mv "$NVIM_DIR" "${NVIM_DIR}.backup"
+                echo "Backup of nvim directory created at $NVIM_DIR.backup."
+            fi
+            install
+            ;;
+        3)
+            echo "Operation aborted."
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice."
+            ;;
+    esac
+else
+    install
+fi
+
 ./create-simlink.sh
